ebc768210b68180a73eeafda7be4c655a0b689d8
arinakabanets <98942941+arinakabanets@users.noreply.github.com>
2022-03-14 11:52:15 +0700

    Divide and multiply functions added (#1)

---
+++ divide.js
@@ -0,0 +1,13 @@
+/**
+ * Divides two values
+ * @param {number} x 
+ * @param {number} y 
+ * @returns x / y
+ */
+function divide(x, y) {
+    return x / y;
+}
+let z;
+z = divide(6,7);
+console.log(z);
+
---
+++ example.js
@@ -0,0 +1,19 @@
+/**
+ * Multiplies two values and prints result
+ * @param {number} x 
+ * @param {number} y 
+ * @returns x * y
+ */
+function multiply(x, y) {
+    let result = x * y;
+    console.log(`Multiplying ${x} by ${y} = ${result}`);
+    return result;
+}
+
+let x = 10;
+let y = 6;
+ 
+//console.log("Mulltiplying x by y = ");
+//console.log("Multiplying " + x + " by " + y + " = ");
+
+multiply(3, 7);
